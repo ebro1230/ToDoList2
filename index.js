94a922b0cb68a002865425e873f0eee77cd0e7bc
ebro1230 <ebro1230@gmail.com>
2023-05-08 20:44:00 +0200

    Just Ed's To Do List

--- index.js
+++
@@ -1,5 +0,0 @@
-let btnopen = document.getElementById("search1");
-let input = document.getElementById("basic-addon2");
-btnopen.addEventListener("click", () => {
-    location.href = 'https://google.com';
-});
